c3c35339e0b076434436bfe2ccff87095ed5aacb
Cade Gordon <cadegordon093@gmail.com>
2021-09-24 11:55:06 -0500

    added player class and game class template

--- HelloDungeonAssesment/Game.cs
+++ HelloDungeonAssesment/Game.cs
@@ -6,9 +6,86 @@ namespace HelloDungeonAssesment
 {
     class Game
     {
+        private bool _gameOver;
+
         public void Run()
         {
+            Start();
 
+            while (!_gameOver)
+            {
+                Update();
+            }
+
+            End();
+        }
+
+        private void Start()
+        {
+            
+        }
+
+        public void End()
+        {
+           
+        }
+
+        private void Update()
+        {
+            
+        }
+
+        public void InitializeItems()
+        {
+
+        }
+
+
+        int GetInput(string description, params string[] options)
+        {
+            string input = "";
+            int inputRecieved = -1;
+
+            while (inputRecieved == -1)
+            {
+                //Print options
+                Console.WriteLine(description);
+                for (int i = 0; i < options.Length; i++)
+                {
+                    Console.WriteLine((i + 1) + ". " + options[i]);
+                }
+                Console.WriteLine("> ");
+
+                //get input from player
+                input = Console.ReadLine();
+
+                //If the player typed an int...
+                if (int.TryParse(input, out inputRecieved))
+                {
+                    //...decrement the input and check if it's within the bounds of the array
+                    inputRecieved--;
+                    if (inputRecieved < 0 || inputRecieved >= options.Length)
+                    {
+                        //set input recieved to be the default value
+                        inputRecieved = -1;
+                        //display error message
+                        Console.WriteLine("Invalid Input");
+                        Console.ReadKey(true);
+                    }
+                }
+                //if the player didnt type and int
+                else
+                {
+                    //Set input recieved to be the default value
+                    inputRecieved = -1;
+                    Console.WriteLine("inavlid input");
+                    Console.ReadKey(true);
+                }
+
+                Console.Clear();
+            }
+
+            return inputRecieved;
         }
     }
 }
---
+++ HelloDungeonAssesment/Player.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloDungeonAssesment
+{
+    class Player
+    {
+    }
+}
